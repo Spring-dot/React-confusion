c2b26c96e78c1139234eb80ec5f2c4d483a23e5b
Akanksha Priya <akankshapriya2162000@gmail.com>
2020-09-05 17:10:54 +0530

    added components 1

--- react/confusion/src/App.js
+++ react/confusion/src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand, NavbarText, NavbarToggler } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
+      <Navbar dark color="secondary">
         <div className="container">
           <NavbarBrand href="/">
             My Tribe
           </NavbarBrand>
+          <NavbarToggler className="justify-content-end">
+            <NavbarText>
+              Signed in as: <a href="/">Akanksha</a>
+            </NavbarText>
+          </NavbarToggler>
+          
+          
         </div>
 
       </Navbar>
+      <Menu />
     </div>
+    
   );
 }
 
